a524ee13bcc838335111d6c8f4ccaddb948d1655
An <82987608+AngieDutra@users.noreply.github.com>
2024-11-12 23:08:26 -0300

    add light collapse icons and toggle overflow (#31)

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -7,17 +7,30 @@ menuBtns.forEach((button) => {
   button.addEventListener("click", () => {
     navOverlay.classList.toggle("hidden");
     navOverlay.classList.toggle("flex");
+    toggleScrollOnNav();
   });
 });
 closeBtns.forEach((button) => {
   button.addEventListener("click", () => {
     navOverlay.classList.toggle("hidden");
     navOverlay.classList.toggle("flex");
+    toggleScrollOnNav();
   });
 });
 navOptions.forEach((option) => {
   option.addEventListener("click", () => {
     navOverlay.classList.toggle("hidden");
     navOverlay.classList.toggle("flex");
+    toggleScrollOnNav();
   });
 });
+
+function toggleScrollOnNav() {
+  const body = document.querySelector("body");
+  const isNavOpen = navOverlay.classList.contains("flex");
+  if (isNavOpen) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "visible";
+  }
+}
