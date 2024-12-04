63cc2e00a037112a55cce0cddb5fccf9ef391507
AngieDutra <angiedutraa@gmail.com>
2024-12-04 02:00:53 -0300

    add new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   darkMode: "selector",
   content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      display: ["League", "sans-serif"],
+      body: ["Lato", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
